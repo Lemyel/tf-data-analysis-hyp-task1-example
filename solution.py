2616647fb425a881329dc6de9abae0c17c059ead
Lemyel <88233153+Lemyel@users.noreply.github.com>
2024-05-18 12:45:05 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,13 +2,17 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 280785885 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    p1 = x_success / x_cnt
+    p2 = y_success / y_cnt
+    p_combined = (x_success + y_success) / (x_cnt + y_cnt)
+    difference = p2 - p1
+    z_value = difference / ((p_combined * (1 - p_combined) * (1/x_cnt + 1/y_cnt)) ** 0.5)
+    z_critical = 1.96
+    
+    return z_value > z_critical
